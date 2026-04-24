e39f0a16b52847126af35c47a149000712281a86
amitpc83 <achowdhary@outlook.com>
2026-04-24 15:45:41 +0200

    Add API project with models and initial migration

---
+++ server/Expenses.Api/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using Expenses.Api.Models.Base;
+
+namespace Expenses.Api.Models
+{
+    public class Transaction : BaseEntity
+    {
+        public string Type { get; set; } = default!;
+        public double Amount { get; set; }
+        public string Category { get; set; } = default!;
+    }
+}
