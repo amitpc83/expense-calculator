a66b65931bb5f0ce8ae0b1b04ccf55d3d72b5abb
amitpc83 <achowdhary@outlook.com>
2026-04-24 16:45:07 +0200

    Add Transaction controller and service layer

---
+++ server/Expenses.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,61 @@
+﻿using Expenses.Api.Dtos;
+using Expenses.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController(ITransactionService transactionService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var transactions = await transactionService.GetAll();
+            return Ok(transactions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var transaction = await transactionService.Get(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            return Ok(transaction);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateTransactionDto payload)
+        {
+            var transaction = await transactionService.Create(payload);
+            return Ok(transaction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id,PutTransactionDto payload)
+        {
+            var transaction = await transactionService.Update(id, payload);
+            if(transaction == null)
+            {
+                return NotFound();
+            }
+            return Ok(transaction);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var transaction = await transactionService.Get(id);
+            if(transaction == null)
+            {
+
+                return NotFound();
+            }
+            await transactionService.Delete(id);
+            return Ok();
+        }
+    }
+}
